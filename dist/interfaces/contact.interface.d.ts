ce2e61194d3c94cd977fdd3a28d3d7ee4eaedea7
Muhammad Sani <jamiludeensani@gmail.com>
2021-10-18 02:14:07 +0100

    server v1.0.1

--- dist/interfaces/contact.interface.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Contact {
-    _id?: string;
-    name: string;
-    email: string;
-    phone: string;
-    status: string;
-    message: string;
-    service: string;
-}
--- dist/models/contact.model.d.ts
+++
@@ -1,4 +0,0 @@
-import { Contact } from '../interfaces/contact.interface';
-import { Document } from 'mongoose';
-declare const contactModel: import("mongoose").Model<Contact & Document<any, any, any>, {}, {}>;
-export default contactModel;
