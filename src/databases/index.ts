fc8dff7d6298b4d5d857f70d746f5168b620d750
Muhammad Sani <jamiludeensani@gmail.com>
2021-10-20 16:25:58 +0100

    server 1.0.5

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,10 +1,10 @@
-import config from 'config';
-import { dbConfig } from '@interfaces/db.interface';
+// import config from 'config';
+// import { dbConfig } from '@interfaces/db.interface';
 
-const { host, port, database }: dbConfig = config.get('dbConfig');
+// const { host, port, database }: dbConfig = config.get('dbConfig');
 
 export const dbConnection = {
-  url: `mongodb://${host}:${port}/${database}`,
+  url: `mongodb+srv://admin-deeni:muha234mmad@cluster0.n2lhm.mongodb.net/mediaplusDB`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
