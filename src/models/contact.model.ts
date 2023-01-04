7ba6013f108f616c595456be35ae61e82de0cb00
Muhammad <jamiludeensani@gmail.com>
2023-01-04 14:27:20 +0100

    new update

--- src/models/contact.model.ts
+++ src/models/contact.model.ts
@@ -1,15 +1,18 @@
 import { Contact } from '@/interfaces/contact.interface';
-import {Schema, model, Document} from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 
 // The Contact page schema
-const contactSchema= new Schema({
+const contactSchema = new Schema(
+  {
     name: String,
     email: String,
     phone: String,
-    status:String,
+    status: String,
     service: String,
-    message: String 
-});
+    message: String,
+  },
+  { timestamps: true },
+);
 
-const contactModel = model<Contact & Document>("Contact", contactSchema);
+const contactModel = model<Contact & Document>('Contact', contactSchema);
 export default contactModel;
