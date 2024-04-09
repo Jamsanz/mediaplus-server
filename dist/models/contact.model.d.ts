13ff82c09fbb56c733daed28fec171c65a04e326
Muhammad <jamiludeensani@gmail.com>
2024-04-10 02:15:56 +0300

    dist and node_modules removed

--- dist/models/contact.model.d.ts
+++
@@ -1,4 +0,0 @@
-import { Document } from 'mongoose';
-import { Contact } from '../interfaces/contact.interface';
-declare const contactModel: import("mongoose").Model<Contact & Document<any, any, any>, {}, {}>;
-export default contactModel;
--- dist/models/stakeholder.model.d.ts
+++
@@ -1,4 +0,0 @@
-import { Document } from 'mongoose';
-import { IStakeholder } from '../interfaces/stakeholder.interface';
-declare const _default: import("mongoose").Model<IStakeholder & Document<any, any, any>, {}, {}>;
-export default _default;
